94e2dd129bbdcec8f38275695e7789aaf1e00780
huoguozhang <zhanglijianwork@163.com>
2019-07-17 16:56:55 +0800

    请求模块添加测试

---
+++ test/requests.spec.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse, AxiosError } from '../src/index'
+import { getAjaxRequest } from './helper'
+
+describe('requests', () => {
+  beforeEach(() => {
+    jasmine.Ajax.install()
+  })
+
+  afterEach(() => {
+    jasmine.Ajax.uninstall()
+  })
+
+  test('should treat single string arg as url', () => {
+    axios('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('/foo')
+      expect(request.method).toBe('GET')
+    })
+  })
+})
